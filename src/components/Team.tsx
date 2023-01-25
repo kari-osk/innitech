fcd088074500dabbb7bd0e44738603c4a240cdc5
kari-osk <karinaosuka@gmail.com>
2023-01-25 20:54:45 -0300

    Add team component

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { teamData } from '../data'
+import React from "react";
+import { teamData } from "../data";
 
 export const Team = () => {
   return (
-    <div className="bg-dark">
-      {teamData.map(team =>(
+    <div className="container bg-dark pb-12 text-light">
+      {teamData.map((team) => (
         <div key={team.id}>
-        <h2>{team.title}</h2>
-        <p>{team.description}</p>
-        <h3>{team.subtitle}</h3>
+          <h2>{team.title}</h2>
+          <p className="pt-6">{team.description}</p>
         </div>
       ))}
+      <div className="flex flex-row pt-6">
+        <h3 className="text-5xl leading-none font-semibold">
+          meet the team
+        </h3>
+        <button>----</button>
+      </div>
     </div>
-  )
-}
+  );
+};
