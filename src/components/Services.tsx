a6eef0484f6bd7c1af16d9d6a93270ce50defa25
kari-osk <karinaosuka@gmail.com>
2023-01-25 20:17:13 -0300

    Add services component

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { servicesData } from "../data";
 
 export const Services = () => {
   return (
-    <>
-      <h2>Services INNI TECH</h2>
-      
-      <section>
-        <h3></h3>
-        <p>
-        </p>
-      </section>
-    </>
+    <div className="container pt-6 mb-12 bg-dark">
+      {servicesData.map((service) => (
+        <div key={service.id} className="w-full text-light ">
+          <h2>{service.title}</h2>
+          <section>
+            <h3>{service.subtitle}</h3>
+            <p>{service.description}</p>
+          </section>
+          <div className="h-[1px] w-full mb-6 mt-6 bg-light" />
+        </div>
+      ))}
+    </div>
   );
 };
--- src/data.tsx
+++ src/data.tsx
@@ -12,12 +12,14 @@ export const aboutData = [
     title: "About INNI TECH",
     description:
       "A company that specializes in creating user-centric digital experiences. Our team of experts in UI and UX research work closely with our clients to understand their unique needs and design software that is tailored to their specific audience.",
-    image: 'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    image:
+      "https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
     alt: "People working with computers",
-    image1: "https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    alt1:"Woman and men siting in from a monitor showing code",
+    image1:
+      "https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    alt1: "Woman and men siting in from a monitor showing code",
     image2: "",
-    alt2: ""
+    alt2: "",
   },
 ];
 
@@ -31,6 +33,7 @@ export const servicesData = [
     subtitle: "UI/UX Research",
     description:
       "Our team of experts in user interface and user experience research will work closely with you to understand your target audience and design software that is intuitive and enjoyable to use.",
+    line: true,
   },
   {
     id: 2,
@@ -53,27 +56,27 @@ export const servicesData = [
 ];
 
 export const teamData = [
-    {
-        id: 0,
-        title: "Team INNI TECH",
-        description: "Experienced designers, developers, and quality assurance specialists who are dedicated to creating user-centric software solutions. Our team is led by a CEO and CTO who have a deep understanding of the latest design trends and technologies."
-    },
-    {
-      id: 1,
-      subtitle: "meet the team",
-    }
-]
-
+  {
+    id: 0,
+    title: "Team INNI TECH",
+    description:
+      "Experienced designers, developers, and quality assurance specialists who are dedicated to creating user-centric software solutions. Our team is led by a CEO and CTO who have a deep understanding of the latest design trends and technologies.",
+  },
+  {
+    id: 1,
+    subtitle: "meet the team",
+  },
+];
 
 export const contact = [
   {
     id: 0,
     title: "From challenges to solutions, with the power of human creativity",
-    button: "Book a meeting"
+    button: "Book a meeting",
   },
   {
     id: 1,
     title: "Contact INNI TECH",
-     subtitle: "Let's talk"
-  }
-] 
+    subtitle: "Let's talk",
+  },
+];
