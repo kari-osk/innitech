ce6a248cd6c83a06971ea66e64cfaed564616ab2
kari-osk <karinaosuka@gmail.com>
2023-02-01 21:57:59 -0300

    Alter z-index

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,9 +5,9 @@ import bg from "../assets/bg.svg";
 export const Hero = () => {
   return (
     <div id="hero" className="w-full h-screen flex items-center relative">
-      <img src={bg} alt="geometric forms in orange colors" className="w-full z-00"  />
+      <img src={bg} alt="geometric forms in orange colors" className="w-full"  />
       {heroData.map((hero) => (
-        <div className="w-full absolute flex flex-col h-screen items-center justify-center z-50">
+        <div className="w-full absolute flex flex-col h-screen items-center justify-center ">
           <p className="w-[270px] md:w-[382px] md:text-2xl text-base self-end pr-4 mb-4">
             {hero.description}
           </p>
